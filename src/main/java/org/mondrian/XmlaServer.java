e87342cdd2980e8964811d9b221f286c87e87e3c
Peter Gillard-Moss <peter@gillardmoss.me.uk>
2014-03-11 15:43:34 +0000

    Start up the mondrian XMLA server

--- src/main/java/org/mondrian/XmlaServer.java
+++ src/main/java/org/mondrian/XmlaServer.java
@@ -1,32 +1,19 @@
 package org.mondrian;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-import java.io.IOException;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
 
-public class XmlaServer extends AbstractHandler
+public class XmlaServer
 {
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response)
-        throws IOException, ServletException
-    {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Mondrian XML/A Server</h1>");
-    }
-
     public static void main(String[] args) throws Exception
     {
-        Server server = new Server(8080);
-        server.setHandler(new XmlaServer());
+        Server server = new Server(8888);
+
+        String rootPath = "/home/vagrant/mondrian-xmla-spike/";
+        WebAppContext webapp = new WebAppContext(rootPath + "src/main/webapp", "");
+        server.setHandler(webapp);
 
         server.start();
+        server.dump(System.err);
         server.join();
     }
 }
